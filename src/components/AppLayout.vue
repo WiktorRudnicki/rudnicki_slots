bc13a7dbd9e58c501be2f64b8978b7c2e462fcf7
WiktorRudnicki <rudnicki.w01@htlwienwest.at>
2021-10-13 14:25:03 +0200

    Unfertige Aufgabe 2
    Die Reihenfolge ist egal

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="layout">
+        <header class="crazy-diamond"><slot name="header"></slot></header>
+        <main class="dogs"><slot></slot></main>
+        <footer class="crazy-diamond"><slot name="footer"></slot></footer>
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+.layout {
+    display: flex;
+};
+.crazy-diamond{
+    text-align: center;
+    padding: 15px;
+    background-color: green;
+};
+.brick-in-the-wall{
+    text-align: center;
+    padding: 15px;
+    background-color: green;
+    margin-top: 60%;
+};
+.dogs{
+    height: 20%;
+    width: 20%;
+    padding: 10px;
+    border: 5px solid black;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,27 @@
 <template>
-  <div style="display: flex">
-    <Sheet style="margin-left: 20px">
-      <h3>Memo to myself</h3>
-      <p>Always prepare for PLFs in time!</p>
-      <img src="info.svg" width="80px" />
-    </Sheet>
-    <Sheet style="margin-left: 20px">
-      <p>INSY, SEW and Webtechnologies are our favorite subjects!</p>
-      <span style="font-style: italic">Starkschwitz Fiedler, Jan Schreck</span>
-      <p>Wish you were here</p>
-      <span style="font-style: italic">E. Wahn</span>
-    </Sheet>
+  <div class="layout">
+    <AppLayout>
+      <AppButton><img src="home.svg" /> Home</AppButton>
+    </AppLayout>
   </div>
 </template>
 
 <script>
-import Sheet from '../components/Sheet.vue';
+import AppLayout from '../components/AppLayout.vue';
+import AppButton from '../components/AppButton.vue';
 
 export default {
   name: 'App',
   components: {
-    Sheet,
+    AppButton,
+    AppLayout,
   },
   data: () => ({}),
 };
 </script>
+
+<style>
+.layout {
+  display: flex;
+}
+</style>
